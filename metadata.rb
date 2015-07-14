b384a3d7ed6eca0e6a1a04a126994d59baddb0cb
Rob Gindes <rob.gindes@gmail.com>
2015-07-14 14:33:47 -0400

    GDAES-1485 forgot version bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'fhanson@gannett.com'
 license          'All rights reserved'
 description      'Installs/Configures cloudwatch-linux'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 depends          'ark'
